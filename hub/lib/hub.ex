77aadb5a5c0ef94eb8167a9d248551dde58e6e0d
Artur Pañach <arturictus@gmail.com>
2017-10-21 06:01:45 +0800

    finished hub

--- hub/lib/hub.ex
+++ hub/lib/hub.ex
@@ -1,18 +1,20 @@
 defmodule Hub do
-  @moduledoc """
-  Documentation for Hub.
-  """
+  HTTPotion.start
+  @username "arturictus"
 
-  @doc """
-  Hello world.
+  "https://api.github.com/users/#{@username}/repos"
+  |> HTTPotion.get(headers: ["User-Agent": "Elixir"])
+  |> Map.get(:body)
+  |> Poison.decode!
+  |> Enum.each(fn repo ->
+       def unquote(String.to_atom(repo["name"]))() do
+         unquote(Macro.escape(repo))
+       end
+     end)
 
-  ## Examples
-
-      iex> Hub.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def go(repo) do
+    url = apply(__MODULE__, repo, [])["html_url"]
+    IO.puts "Luaching browser to #{url}..."
+    System.cmd("open", [url])
   end
 end
